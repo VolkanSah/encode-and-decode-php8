38b9ee679d2c56b64245e13d957cdbde727f5e5b
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-02-25 23:11:23 +0100

    Create encoding.php

---
+++ encoding.php
@@ -0,0 +1,13 @@
+
+// Read file contents into a string
+$fileContents = file_get_contents('path/to/file.txt');
+
+// Generate a random key and nonce
+$key = sodium_crypto_secretbox_keygen();
+$nonce = random_bytes(SODIUM_CRYPTO_SECRETBOX_NONCEBYTES);
+
+// Encrypt the file contents with the key and nonce
+$encryptedContents = sodium_crypto_secretbox($fileContents, $nonce, $key);
+
+// Write the encrypted contents to a file
+file_put_contents('path/to/encrypted-file.bin', $nonce . $encryptedContents . $key);
